e4d81336ad0714fb96bde0ef8b7e0f9bc34707ff
Quintin <quintin@zaplingstudios.com>
2022-02-28 10:28:24 -0500

    Finished implementing AsyncImage with images based on state and a completion animation.

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Quintin Rodriguez-Harrison on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
